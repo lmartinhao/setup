aa434f96efc17b4056c4d6cf8f4035e7b94d3ba3
lola martinhao <lola.martinhao@gmail.com>
2023-01-18 18:28:57 -0300

    header created

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,28 @@
 import './styles/global.css';
+import { Plus } from 'phosphor-react';
 
-import { Habit } from "./components/Habit"
+//import { Habit } from "./components/Habit"
 
-function App() {
+import logoImage from './assets/logo.svg';
+
+export function App() {
 
   return (
-    <div>
-      <Habit completed={4} />
-      <Habit completed={12} />
-      <Habit completed={15} />
-      <Habit completed={21} />
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+
+        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+          <img src={logoImage} alt='logotipo' />
+          <button
+            type='button'
+            className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
+          >
+            <Plus size={20} className="text-violet-500"/>
+            Novo hábito
+          </button>
+        </div>
+
+      </div>
     </div>
   )
 }
-
-export default App
