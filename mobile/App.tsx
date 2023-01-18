641137a6c8d0ffc11ead06b07396ac9a498a8240
lola martinhao <lola.martinhao@gmail.com>
2023-01-18 13:56:56 -0300

    adding loading components and custom fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+ } from '@expo-google-fonts/inter';
+
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if(!fontsLoaded) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Oi!</Text>
@@ -13,11 +35,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
     color: '#fff',
+    fontFamily: 'Inter_800ExtraBold'
   }
 });
