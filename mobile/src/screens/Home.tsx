9bc4d5ed464d8d8be991d53ff00961b66c0bb905
lola martinhao <lola.martinhao@gmail.com>
2023-01-18 21:13:15 -0300

    habit day component created

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,9 +1,31 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
+import { Header } from "../components/Header";
 
 export function Home() {
   return (
-    <View className="">
+    <View className="flex-1 bg-background px-8 pt-16">
+      <Header />
 
+      <View className="flex-row mt-6 mb-2">
+        {
+          weekDays.map((weekDay, i) => (
+            <Text
+              key={i}
+              className="text-zinc-400 text-xl font-bold text-center mx-1"
+              style={{ width: DAY_SIZE }}
+            >
+              {weekDay}
+            </Text>
+          ))
+
+        }
+      </View>
+
+      <HabitDay />
     </View>
   )
 }
