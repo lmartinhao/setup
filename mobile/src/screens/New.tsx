91e4817e0821014ddbb0b1302ba247d56af0ea7d
lola martinhao <lola.martinhao@gmail.com>
2023-01-19 17:59:37 -0300

    confirm button finished

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { ScrollView, Text, TextInput, View } from "react-native";
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Feather } from '@expo/vector-icons';
+import colors from "tailwindcss/colors";
 
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
@@ -27,7 +29,10 @@ export function New() {
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 400 }}
+      >
 
         <BackButton />
 
@@ -40,6 +45,8 @@ export function New() {
         </Text>
         <TextInput
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
+          placeholder="ex.: Beber 2L de água, estudar 1h, etc..."
+          placeholderTextColor={colors.zinc[400]}
         />
 
         <Text className="mt-4 mb-3 text-white font-semibold text-base">
@@ -58,6 +65,20 @@ export function New() {
             )
           })
         }
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+        >
+          <Feather
+            name="check"
+            size={20}
+            color={colors.white}
+          />
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirmar
+          </Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
