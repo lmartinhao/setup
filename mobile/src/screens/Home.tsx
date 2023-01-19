b0bac897a15743e58fe3fe5484e26ebf3562256e
lola martinhao <lola.martinhao@gmail.com>
2023-01-18 21:36:45 -0300

    adding dayjs

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -48,6 +48,7 @@ export function Home() {
             .from({ length: amountOfDaysToFill })
             .map((_, i) => (
               <View
+                key={i}
                 className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
                 style={{ width: DAY_SIZE, height: DAY_SIZE }}
               />
