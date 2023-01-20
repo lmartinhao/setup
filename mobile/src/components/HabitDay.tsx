cc7d62d64b69dcae2867f0a940d752b7abd27365
lola martinhao <lola.martinhao@gmail.com>
2023-01-20 18:47:14 -0300

    adding style based on data ate habit component

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,4 +1,7 @@
 import { TouchableOpacity, Dimensions, TouchableOpacityProps } from "react-native";
+import clsx from 'clsx';
+
+import { generateProgressPercentage } from "../utils/generate-progress-percentage";
 
 const WEEK_DAYS = 7;
 const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
@@ -13,9 +16,20 @@ interface Props extends TouchableOpacityProps {
 }
 
 export function HabitDay({ amountOfHabits = 0, amountCompleted = 0, date, ...rest }: Props) {
+  const amountAccomplishedPercentage = amountOfHabits > 0 ? generateProgressPercentage(amountOfHabits, amountCompleted) : 0;
+
   return (
     <TouchableOpacity
-      className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+      className={clsx("rounded-lg border-2 m-1", {
+        ["bg-zinc-900 border-zinc-800"] : amountAccomplishedPercentage === 0,
+        ["bg-violet-900 border-violet-700"] : amountAccomplishedPercentage > 0 && amountAccomplishedPercentage < 20,
+        ["bg-violet-800 border-violet-600"] : amountAccomplishedPercentage > 20 && amountAccomplishedPercentage < 40,
+        ["bg-violet-700 border-violet-500"] : amountAccomplishedPercentage > 40 && amountAccomplishedPercentage < 60,
+        ["bg-violet-600 border-violet-500"] : amountAccomplishedPercentage > 60 && amountAccomplishedPercentage < 80,
+        ["bg-violet-500 border-violet-400"] : amountAccomplishedPercentage > 80,
+
+
+      })}
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
       activeOpacity={0.7}
       { ...rest }
